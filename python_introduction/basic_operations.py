a351c236d66346ef5251033831f56da99f6d2b26
Mahmoud Elad <mahmoudeladl@outlook.com>
2025-06-01 21:36:31 +0300

    first submmison

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,10 +1,8 @@
-number1 = 10
-number2 = 5
-
-sum_result = number1 + number2
-difference_result = number1 - number2
-product_result = number1 * number2
-
-print(f"Addition of {number1} and {number2} is {sum_result}")
-print(f"Subtraction of {number1} and {number2} is {difference_result}")
-print(f"Multiplication of {number1} and {number2} is {product_result}")
+number1=10 
+number2=5
+sum=number1+number2
+print("Addition of 10 and 5 is" ,sum)
+substact=number1-number2
+print("Subtraction of 10 and 5 is ",substact)
+product=number1*number2
+print("Multiplication of 10 and 5 is",product)
